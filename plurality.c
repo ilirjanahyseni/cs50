ddcef84f21838c3b53c40b8a5336cd96cba92c06
Ilirjana Hyseni <92699878+ilirjanahyseni@users.noreply.github.com>
2023-11-17 13:28:25 -0600

    Add files via upload

---
+++ plurality.c
@@ -0,0 +1,110 @@
+/*
+Objective:
+    This program runs a plurality election and declares the candidate with the greatest number of votes the winner(s)
+    It uses command-line arguments ('argc' and argv') to determine the candidate(s)' name(s) at runtime instead of hardcoding their names into the program.
+    However, the number of individual voters and the votes cast by each voter are provided as a user input.
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+} candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    // Update existing candidae's vote total
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // If the names match
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    int max_votes = 0;
+    //  Find the candidate with maximum votes
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > max_votes)
+        {
+            // Update max_vote if necessary to contain highest count
+            max_votes = candidates[i].votes;
+        }
+    }
+    // Print candidates who have the most votes
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+    return;
+}
